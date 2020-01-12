9115e5a954fbdc817ebb79a7d8554ed4e047d999
isao <feat130@hotmail.co.jp>
2020-01-12 11:59:27 -0800

    Add the global state to Game.jsx

--- frontend/src/components/views/Game/Game.jsx
+++ frontend/src/components/views/Game/Game.jsx
@@ -1,59 +1,21 @@
-import React, {useState} from 'react';
-import { Provider } from "react-redux";
-
-import store from '../../../store';
+import React, { useState } from 'react';
 import PlayerField from './PlayerField/PlayerField';
 import OpponentFieldMap from './OpponentFieldMap/OpponentFieldMap';
 import Keyboard from './Keyboard/Keyboard';
 
-const dummy = {
-  multipliers: [[2, 4, 5, 2, 3, 5, 6, 8, 9, 3], [2, 4, 5, 2, 3, 5, 6, 8, 9, 3], [2, 4, 5, 2, 3, 5, 6, 8, 9, 3]],
-  players: [
-    {
-      name: "Vlad",
-      progress: 0,
-      lastLine: 10,
-      state: {
-        multiply: 4,
-        transfer: true,
-        add: 0,
-        remove: 0
-      }
-    },
-    {
-      lastLine: 10,
-      name: "Shota",
-      progress: 7,
-      state: {
-        multiply: 0,
-        transfer: false,
-        add: 2,
-        remove: 1
-      }
-    },
-    {
-      lastLine: 10,
-      name: "Isao",
-      progress: 3,
-      state: {
-        multiply: 2,
-        transfer: true,
-        add: 0,
-        remove: 5
-      }
-    }
+function Game() {
+  const gameState = {
+    multipliers: useSelector(state => state.multipliers),
+    players: useSelector(state => state.players)
+  }
 
-  ]
-}
-
-function Game(){
-  return(
+  return (
     <>
       {true ? (
         <>
           <PlayerField />
-          <OpponentFieldMap gameState={dummy}/>
-          <Keyboard/>
+          <OpponentFieldMap gameState={gameState} />
+          <Keyboard />
         </>
       ) : (<h1>Loading...</h1>)}
     </>

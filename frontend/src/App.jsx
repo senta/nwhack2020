fbdae8929d47286ee9bb0253cb93e09f92ee2b84
unknown <vladi00345@gmail.com>
2020-01-11 21:03:03 -0800

    Added basic layout to the field map

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, {useState} from 'react';
 import PlayerField from './components/views/PlayerField/PlayerField';
 import OpponentFieldMap from './components/views/OpponentFieldMap/OpponentFieldMap';
 import Keyboard from './components/views/Keyboard/Keyboard';
@@ -6,12 +6,48 @@ import io from 'socket.io-client';
 
 const socket = io();
 
+const dummy = {
+  multipliers: [[2,4,5,2,3,5,6,8,9,3], [2,4,5,2,3,5,6,8,9,3], [2,4,5,2,3,5,6,8,9,3]],
+  players: [
+    {
+      name: "Vlad",
+      progress: 0,
+      lastLine: 10,
+      state: {
+        multiply: 4,
+        transfer: true,
+        add: 0,
+        remove: 0
+      }
+    },
+    {
+      lastLine: 10,
+      name: "Shota",
+      progress: 3,
+      state: {
+        multiply: 0,
+        transfer: false,
+        add: 2,
+        remove: 1
+      }
+    }
+  ]
+}
+
 function App() {
+  let [gameState, setGameState] = useState({status: 'start'});
+  socket.on('GAME_START', (data) => {
+    setGameState({status: 'start', ...data});
+  });
   return (
     <div className="App">
-      <PlayerField/>
-      <OpponentFieldMap />
-      <Keyboard socket={socket}/>
+      {gameState.status === 'start' ? (
+        <>
+          <PlayerField/>
+          <OpponentFieldMap gameState={dummy} socket={socket}/>
+          <Keyboard gameState={gameState} socket={socket}/>
+        </>
+      ) : (<h1>Loading...</h1>)}
     </div>
   );
 }
--- frontend/src/components/views/OpponentFieldMap/OpponentFieldMap.jsx
+++ frontend/src/components/views/OpponentFieldMap/OpponentFieldMap.jsx
@@ -1,11 +1,34 @@
 import React from 'react';
 import './OpponentFieldMap.scss';
 
-const OpponentFieldMap = () => {
+const OpponentFieldMap = (props) => {
+  const players = props.gameState.players.map((player,index) => <Opponent key={index} player={player}/>);
   return (
-    <div className="App">
+    <div id="minimap">
+      {players}
     </div>
   );
 };
+
+const Opponent = (props) => {
+  const {name, state, progress, lastLine} = props.player;
+  const finishLine = progress + 4 >= lastLine ? lastLine : progress + 4;
+  const cells = (<>
+    <td/><td/><td/><td/><td/><td/><td/><td/><td/><td/>
+  </>)
+  return (
+    <div>
+      <h2>{name}</h2>
+      {state.multiply ? <span id="multiplier">{state.multiply}x</span> : null}
+      <table>
+        <tr className={progress >= 1 ? 'complete' : 'empty'}>{cells}</tr>
+        <tr className={progress >= 2 ? 'complete' : 'empty'}>{cells}</tr>
+        <tr className={progress >= 3 ? 'complete' : 'empty'}>{cells}</tr>
+        <tr className={progress >= 4 ? 'complete' : 'empty'}>{cells}</tr>
+      </table>
+      <span id="last-line">{finishLine}</span>
+    </div>
+  );
+}
 
 export default OpponentFieldMap;

ab5c724151d41ab36299c3942a15b437c89c2817
unknown <vladi00345@gmail.com>
2020-01-11 16:39:27 -0800

    Added action markup and basic layout styling

--- frontend/src/components/views/Keyboard/Keyboard.jsx
+++ frontend/src/components/views/Keyboard/Keyboard.jsx
@@ -1,27 +1,38 @@
 import React from 'react';
+import './Keyboard.scss';
 
 const Keyboard = () => {
   return (
-    <div className="keyboard">
-      <div>
-        <button onClick={handleClick}>9</button>
-        <button onClick={handleClick}>8</button>
-        <button onClick={handleClick}>7</button>
+    <>
+      <div className="numpad">
+        <div>
+          <button onClick={handleClick}>9</button>
+          <button onClick={handleClick}>8</button>
+          <button onClick={handleClick}>7</button>
+        </div>
+        <div>
+          <button onClick={handleClick}>6</button>
+          <button onClick={handleClick}>5</button>
+          <button onClick={handleClick}>4</button>
+        </div>
+        <div>
+          <button onClick={handleClick}>3</button>
+          <button onClick={handleClick}>2</button>
+          <button onClick={handleClick}>1</button>
+        </div>
+        <div>
+          <button onClick={handleClick}>0</button>
+        </div>
       </div>
-      <div>
-        <button onClick={handleClick}>6</button>
-        <button onClick={handleClick}>5</button>
-        <button onClick={handleClick}>4</button>
+      <div className="actions-left">
+        <button>Multiply</button>
+        <button>Transfer</button>
       </div>
-      <div>
-        <button onClick={handleClick}>3</button>
-        <button onClick={handleClick}>2</button>
-        <button onClick={handleClick}>1</button>
+      <div className="actions-right">
+        <button>Add</button>
+        <button>Remove</button>
       </div>
-      <div>
-        <button onClick={handleClick}>0</button>
-      </div>
-    </div>
+    </>
   );
 };
 
